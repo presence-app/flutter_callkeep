1e84e0dd81d232d95395e0eb6cfbe93e4c512c3d
iosemagno <iose.magno@gmail.com>
2025-09-07 23:07:58 +0200

    Custom Incoming Call
    It comprises from top to bottom:
    1. Avatar (EXTRA_CALLKEEP_AVATAR)
    2. CallerName, eg "John Doe"(EXTRA_CALLKEEP_CALLER_NAME)
    3. ContentTitlem eg "Incoming Call"
    (EXTRA_CALLKEEP_CONTENT_TITLE)
    4. Accept / Decline circle buttons with custom icons
    5. Custom gradient background color

--- flutter_callkeep/android/src/main/kotlin/co/doneservices/callkeep/IncomingCallActivity.kt
+++ flutter_callkeep/android/src/main/kotlin/co/doneservices/callkeep/IncomingCallActivity.kt
@@ -3,7 +3,6 @@ package co.doneservices.callkeep
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.KeyguardManager
-import android.app.KeyguardManager.KeyguardLock
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
@@ -15,51 +14,32 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.view.ViewGroup
 import android.view.Window
 import android.view.WindowManager
 import android.view.animation.AnimationUtils
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import android.widget.Button
 import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.ACTION_CALL_INCOMING
 import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_AVATAR
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_BACKGROUND_URL
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_ACCENT_COLOR
 import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_DURATION
 import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_INCOMING_DATA
 import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_CALLER_NAME
 import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_CONTENT_TITLE
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_HANDLE
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_HEADERS
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_LOGO
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_HAS_VIDEO
 import com.squareup.picasso.Picasso
-import de.hdodenhof.circleimageview.CircleImageView
 import kotlin.math.abs
-import okhttp3.OkHttpClient
-import com.squareup.picasso.OkHttp3Downloader
-import android.view.ViewGroup.MarginLayoutParams
 import android.os.PowerManager
-import android.os.PowerManager.WakeLock
-import android.text.TextUtils
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_ACCEPT_TEXT
-import co.doneservices.callkeep.CallKeepBroadcastReceiver.Companion.EXTRA_CALLKEEP_DECLINE_TEXT
-
 
 class IncomingCallActivity : Activity() {
 
     companion object {
-
-        const val ACTION_ENDED_CALL_INCOMING =
-                "co.doneservices.callkeep.ACTION_ENDED_CALL_INCOMING"
+        const val ACTION_ENDED_CALL_INCOMING = "co.doneservices.callkeep.ACTION_ENDED_CALL_INCOMING"
 
         fun getIntent(context: Context, data: Bundle) = Intent(ACTION_CALL_INCOMING).apply {
             action = "${context.packageName}.${ACTION_CALL_INCOMING}"
             putExtra(EXTRA_CALLKEEP_INCOMING_DATA, data)
-            flags =
-                    Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_REORDER_TO_FRONT
         }
 
         fun getIntentEnded(context: Context): Intent {
@@ -82,14 +62,11 @@ class IncomingCallActivity : Activity() {
     private var endedCallKeepBroadcastReceiver = EndedCallKeepBroadcastReceiver()
 
     private lateinit var llBackground: LinearLayout
-
     private lateinit var tvCallerName: TextView
     private lateinit var tvCallHeader: TextView
     private lateinit var ivLogo: ImageView
-
-    private lateinit var btnAnswer: Button
-
-    private lateinit var btnDecline: Button
+    private lateinit var btnAnswer: ImageButton
+    private lateinit var btnDecline: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -107,47 +84,35 @@ class IncomingCallActivity : Activity() {
         setContentView(R.layout.activity_call_incoming)
         initView()
         updateViewWithIncomingIntentData(intent)
+
+        val filter = IntentFilter("${packageName}.${ACTION_ENDED_CALL_INCOMING}")
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            registerReceiver(
-                endedCallKeepBroadcastReceiver,
-                IntentFilter("${packageName}.${ACTION_ENDED_CALL_INCOMING}"),
-                Context.RECEIVER_EXPORTED,
-            )
+            registerReceiver(endedCallKeepBroadcastReceiver, filter, Context.RECEIVER_EXPORTED)
         } else {
-            registerReceiver(
-                endedCallKeepBroadcastReceiver,
-                IntentFilter("${packageName}.${ACTION_ENDED_CALL_INCOMING}")
-            )
+            registerReceiver(endedCallKeepBroadcastReceiver, filter)
         }
     }
 
     private fun wakeLockRequest(duration: Long) {
-
         val pm = applicationContext.getSystemService(POWER_SERVICE) as PowerManager
         val wakeLock = pm.newWakeLock(
-                PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.FULL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
-                "CallKeep:PowerManager"
+            PowerManager.SCREEN_BRIGHT_WAKE_LOCK or PowerManager.FULL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+            "CallKeep:PowerManager"
         )
         wakeLock.acquire(duration)
     }
 
     private fun setTransparentStatusAndNavigation() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            setWindowFlag(
-                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                            or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, true
-            )
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, true)
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setWindowFlag(
-                    (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS
-                            or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION), false
-            )
+            setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, false)
             window.statusBarColor = Color.TRANSPARENT
             window.navigationBarColor = Color.TRANSPARENT
         }
@@ -155,15 +120,10 @@ class IncomingCallActivity : Activity() {
 
     private fun setWindowFlag(bits: Int, on: Boolean) {
         val win: Window = window
-        val winParams: WindowManager.LayoutParams = win.attributes
-        if (on) {
-            winParams.flags = winParams.flags or bits
-        } else {
-            winParams.flags = winParams.flags and bits.inv()
-        }
+        val winParams = win.attributes
+        winParams.flags = if (on) winParams.flags or bits else winParams.flags and bits.inv()
         win.attributes = winParams
     }
-
 
     private fun updateViewWithIncomingIntentData(intent: Intent) {
         val data = intent.extras?.getBundle(EXTRA_CALLKEEP_INCOMING_DATA)
@@ -172,76 +132,58 @@ class IncomingCallActivity : Activity() {
         tvCallerName.text = data?.getString(EXTRA_CALLKEEP_CALLER_NAME, "")
         tvCallHeader.text = data?.getString(EXTRA_CALLKEEP_CONTENT_TITLE, "")
 
-
-        val logo = data?.getString(EXTRA_CALLKEEP_LOGO, "")
-        if(logo?.isNotEmpty() == true){
-            val identifier = this.resources.getIdentifier(logo, "drawable", this.packageName)
-            ivLogo.setImageResource(identifier)
+        // Display avatarUrl
+        val avatarUrl = data?.getString(EXTRA_CALLKEEP_AVATAR)
+        val avatarDrawable = if (!avatarUrl.isNullOrEmpty()) {
+            avatarUrl
+        } else {
+            null
         }
-        ivLogo.visibility = if (logo?.isNotEmpty() == true) View.VISIBLE else View.INVISIBLE
 
-
-        val hasVideo = data?.getBoolean(EXTRA_CALLKEEP_HAS_VIDEO, false) ?: false
-        if (hasVideo) {
-            val top = getResources().getDrawable(R.drawable.ic_video);
-            btnAnswer.setCompoundDrawablesWithIntrinsicBounds(null, top, null, null);
+        // If avatarUrl is not available, display default ic_default_avatar
+        ivLogo.visibility = View.VISIBLE
+        if (avatarDrawable != null) {
+            Picasso.get().load(avatarDrawable).into(ivLogo)
+        } else {
+            ivLogo.setImageResource(R.drawable.ic_default_avatar)
         }
+
         val duration = data?.getLong(EXTRA_CALLKEEP_DURATION, 0L) ?: 0L
         wakeLockRequest(duration)
-
         finishTimeout(data, duration)
+        applyGradientBackground()
+    }
 
-        val accentColor = data?.getString(EXTRA_CALLKEEP_ACCENT_COLOR, "#0955fa")
-        try {
-            llBackground.setBackgroundColor(Color.parseColor(accentColor))
-        } catch (error: Exception) {
-        }
-
+    private fun applyGradientBackground() {
+        val colors = intArrayOf(Color.parseColor("#456D91"), Color.parseColor("#2A5379"))
+        val gradient = android.graphics.drawable.GradientDrawable(android.graphics.drawable.GradientDrawable.Orientation.TOP_BOTTOM, colors)
+        gradient.cornerRadius = 0f
+        llBackground.background = gradient
     }
 
     private fun finishTimeout(data: Bundle?, duration: Long) {
         val currentSystemTime = System.currentTimeMillis()
-        val timeStartCall =
-                data?.getLong(CallKeepNotificationManager.EXTRA_TIME_START_CALL, currentSystemTime)
-                        ?: currentSystemTime
-
+        val timeStartCall = data?.getLong(CallKeepNotificationManager.EXTRA_TIME_START_CALL, currentSystemTime)
+            ?: currentSystemTime
         val timeOut = duration - abs(currentSystemTime - timeStartCall)
         Handler(Looper.getMainLooper()).postDelayed({
             if (!isFinishing) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    finishAndRemoveTask()
-                } else {
-                    finish()
-                }
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) finishAndRemoveTask() else finish()
             }
         }, timeOut)
     }
 
     private fun initView() {
         llBackground = findViewById(R.id.llBackground)
-
         tvCallerName = findViewById(R.id.tvCallerName)
         tvCallHeader = findViewById(R.id.tvCallHeader)
         ivLogo = findViewById(R.id.ivLogo)
-
         btnAnswer = findViewById(R.id.btnAnswer)
         btnDecline = findViewById(R.id.btnDecline)
-        animateAcceptCall()
 
-        btnAnswer.setOnClickListener {
-            onAcceptClick()
-        }
-        btnDecline.setOnClickListener {
-            onDeclineClick()
-        }
+        btnAnswer.setOnClickListener { onAcceptClick() }
+        btnDecline.setOnClickListener { onDeclineClick() }
     }
-
-    private fun animateAcceptCall() {
-        val shakeAnimation =
-                AnimationUtils.loadAnimation(this@IncomingCallActivity, R.anim.shake_anim)
-        btnAnswer.animation = shakeAnimation
-    }
-
 
     private fun onAcceptClick() {
         val data = intent.extras?.getBundle(EXTRA_CALLKEEP_INCOMING_DATA)
@@ -252,48 +194,24 @@ class IncomingCallActivity : Activity() {
             intent?.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
         if (intent != null) {
-            val intentTransparent = TransparentActivity.getIntentAccept(this@IncomingCallActivity, data)
+            val intentTransparent = TransparentActivity.getIntentAccept(this, data)
             startActivities(arrayOf(intent, intentTransparent))
         } else {
-            val acceptIntent = CallKeepBroadcastReceiver.getIntentAccept(this@IncomingCallActivity, data)
+            val acceptIntent = CallKeepBroadcastReceiver.getIntentAccept(this, data)
             sendBroadcast(acceptIntent)
         }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
             keyguardManager.requestDismissKeyguard(this, null)
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            finishAndRemoveTask()
-        } else {
-            finish()
-        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) finishAndRemoveTask() else finish()
     }
 
     private fun onDeclineClick() {
         val data = intent.extras?.getBundle(EXTRA_CALLKEEP_INCOMING_DATA)
-        val intent =
-                CallKeepBroadcastReceiver.getIntentDecline(this@IncomingCallActivity, data)
+        val intent = CallKeepBroadcastReceiver.getIntentDecline(this, data)
         sendBroadcast(intent)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            finishAndRemoveTask()
-        } else {
-            finish()
-        }
-    }
-
-    private fun getPicassoInstance(context: Context, headers: HashMap<String, Any?>): Picasso {
-        val client = OkHttpClient.Builder()
-                .addNetworkInterceptor { chain ->
-                    val newRequestBuilder: okhttp3.Request.Builder = chain.request().newBuilder()
-                    for ((key, value) in headers) {
-                        newRequestBuilder.addHeader(key, value.toString())
-                    }
-                    chain.proceed(newRequestBuilder.build())
-                }
-                .build()
-        return Picasso.Builder(context)
-                .downloader(OkHttp3Downloader(client))
-                .build()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) finishAndRemoveTask() else finish()
     }
 
     override fun onDestroy() {
@@ -302,6 +220,4 @@ class IncomingCallActivity : Activity() {
     }
 
     override fun onBackPressed() {}
-
-
 }
